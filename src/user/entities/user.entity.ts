e4914fbff1609203b5efb06eb23dc1f1f3f9ac07
nimabht <nimabeheshtaein99@gmail.com>
2023-05-14 07:45:40 +0330

    user entity updated

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,27 +1,28 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import * as bcrypt from 'bcrypt';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ unique: true })
+  @Column({ unique: true, nullable: false })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @Column()
+  @Column({ nullable: false })
   @IsNotEmpty()
   @IsString()
   fullname: string;
 
-  @Column()
+  @Column({ nullable: false })
   @IsNotEmpty()
   password: string;
 
-  @Column()
+  @Column({ nullable: false })
   @IsNotEmpty()
   role: string;
 
